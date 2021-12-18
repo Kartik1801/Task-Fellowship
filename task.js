eb318d6f57e9f58d6f9638ffc9217ba9e8828529
Kartik1801 <kartikdhawan1801@gmail.com>
2021-12-18 21:04:38 +0530

    updated

--- task.js
+++ task.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+const fs =  require("fs");
+const path = require("path");
+const args = process.argv.slice(2);
+console.log(args);
+
+const addTask = () =>{
+        
+}
+
+switch(args[0]){
+    case undefined:
+    case "help" : 
+        console.log(`Usage :-
+                $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+                $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+                $ ./task del INDEX            # Delete the incomplete item with the given index
+                $ ./task done INDEX           # Mark the incomplete item with the given index as complete
+                $ ./task help                 # Show usage
+                $ ./task report               # Statistics`);
+        break;
+    case "ls":
+        break;
+    case "add": addTask();
+        break;
+    case "del":
+        break;
+    case "report": 
+        break;                
+}
