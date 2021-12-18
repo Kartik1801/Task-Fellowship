8d688be566b588d7e00d0ebfc777626220a02cc2
Kartik1801 <kartikdhawan1801@gmail.com>
2021-12-18 23:56:16 +0530

    updated

--- task.js
+++ task.js
@@ -2,35 +2,32 @@ const fs = require("fs");
 const path = require("path");
 const args = process.argv.slice(2);
 
-switch(args[0]){
-    case undefined:
-    case "help" : 
-        console.log(`Usage :-
-                $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
-                $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
-                $ ./task del INDEX            # Delete the incomplete item with the given index
-                $ ./task done INDEX           # Mark the incomplete item with the given index as complete
-                $ ./task help                 # Show usage
-                $ ./task report               # Statistics`);
-        break;
-    case "ls":
-        break;
-    case "add": addTask();
-        break;
-    case "del":
-        break;
-    case "report": 
-        break;                
-}
+const help = () => console.log(`Usage :-\n$ ./task add 2 hello world\t\t# Add a new item with priority 2 and text "hello world" to the list\n$ ./task ls\t\t\t\t# Show incomplete priority list items sorted by priority in ascending order\n$ ./task del INDEX\t\t\t# Delete the incomplete item with the given index\n$ ./task done INDEX\t\t\t# Mark the incomplete item with the given index as complete\n$ ./task help\t\t\t\t# Show usage\n$ ./task report\t\t\t\t# Statistics`);
 const addTask = ()=>{
     let tasks=[];
     fs.readFile("task.txt",(err, data)=>{
     if (data) tasks = data.toString().split('\r\n');
      tasks.push(`${args[1]} ${args[2]}`);
-     tasks.sort((a,b)=>{ a=a.split(" ")[0];a=parseInt(a); b=b.split(" ")[0];b=parseInt(b); return (a>b)?1:-1;});
+     tasks.sort((a,b)=>{ a=parseInt(a);b=parseInt(b); return (a>b)?1:-1;});
      fs.writeFile("task.txt",tasks.join('\r\n'), (err) => { 
          if (err) throw err;  
          else console.log(`Added task: "${args[2]}" with priority ${args[1]}`);
      }) 
     })
+}
+const list = ()=>{
+    let tasks=[];
+    fs.readFile("task.txt",(err, data)=>{
+    if (data) tasks = data.toString().split('\r\n');
+     tasks.sort((a,b)=>{a=parseInt(a);b=parseInt(b); return (a>b)?1:-1;});
+     tasks.forEach((d,i)=>{console.log((`${++i}. ${d.match(/[a-zA-Z ]+/g)} [${d.match(/\d+/g)}]`))})
+    })
+}
+switch(args[0]){
+    case undefined:
+    case "help": help(); break;
+    case "ls": list(); break;
+    case "add": addTask(); break;
+    case "del": break;
+    case "report": break;                
 }
